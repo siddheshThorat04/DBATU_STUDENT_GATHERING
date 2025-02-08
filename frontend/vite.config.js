1843057449adefa408af464a7c646f76eff61221
siddheshThorat04 <siddheshthorat2004@gmail.com>
2025-02-08 22:19:45 +0530

    vite file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,19 +3,19 @@ import react from "@vitejs/plugin-react";
 
 
 // const API =  "http://localhost:8080" 
-const API =  "https://dbatu-student-gathering.onrender.com/" 
+const API = "https://dbatu-student-gathering.onrender.com/"
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-		server: {
-			port: 3000,
-			proxy:{
-				"/api": {
-					target:API,
-					changeOrigin: true,
-					secure: true,
-				},
+	server: {
+		port: 3000,
+		proxy: {
+			"/api": {
+				target: API,
+				changeOrigin: true,
+				secure: true,
 			},
 		},
+	},
 });
